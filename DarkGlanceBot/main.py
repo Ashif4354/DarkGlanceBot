b0e3d720b16930e5af237c21b419125f98fa5f69
Ashif4354 <ashif.abhas@gmail.com>
2022-11-20 10:19:22 +0530

    stage 1 finish v2

--- DarkGlanceBot/main.py
+++ DarkGlanceBot/main.py
@@ -15,12 +15,12 @@ async def on_ready():
     print("DarkGlanceBot has been started")
 
 @client.command()
-async def hello(text):
-    await text.send('Hi')
+async def hello(text_channel):
+    await text_channel.send('Hi')
     
 @client.command()   
-async def kcg(text):
-    command = text.message.content.split()
+async def kcg(text_channel):
+    command = text_channel.message.content.split()
     year = '0000'
     try:
         if command[1] == 'photo':
@@ -31,18 +31,20 @@ async def kcg(text):
                     year = command[3] 
                 except:
                     None  
-                            
-                d_o_b = find_dob(user_id, year)                
+                await text_channel.send("Please Wait while we crack the date of birth")           
+                d_o_b = find_dob(user_id, year)   
+                await text_channel.send("DOB FOUND")
+                await text_channel.send("Please wait while we fetch the photo")
                 kcg_student.login(user_id, d_o_b)
                 kcg_student.get_photo(path = os.getcwd() + '\kcg', uid = user_id)
                 
                 photo = "{}\kcg\collected_pics\{}.png".format(os.getcwd(), user_id)
                 with open(photo, 'rb') as f:
                     photo = discord.File(f)
-                    await text.send(file=photo)
+                    await text_channel.send(file=photo)
                 
             else:
-                await text.send("Invalid RegisterNo/RollNo")
+                await text_channel.send("Invalid RegisterNo/RollNo")
     except:
         None       
             
