f4e1a1ec578421c9f2f8a2f677cd4b753e33a72f
Ashif4354 <ashif.abhas@gmail.com>
2022-11-20 07:55:03 +0530

    mad changes to darkglance.py

--- DarkGlanceBot/darkglance.py
+++ DarkGlanceBot/darkglance.py
@@ -3,7 +3,7 @@ import mysql.connector
 mycon = mysql.connector.connect(host="localhost", passwd="rootmysql",user="root")
 mysql_cursor = mycon.cursor()
 
-mysql_cursor.execute('CREATE DATABASE [IF NOT EXISTS] kcg')
+mysql_cursor.execute('CREATE DATABASE IF NOT EXISTS kcg')
 class discord:
     token = 'MTA0MzM4MDA3NTc5MTM4NDU4Ng.G1a8ns.7UbXHuZjH4Ou2T5t8vjUpZIlgCec9qp255fR18'
 
