97f63b6afa11b119f0a93a8f350141fb27f3e084
Ashif4354 <53007623+Ashif4354@users.noreply.github.com>
2022-11-20 07:07:56 +0530

    Add files via upload

---
+++ DarkGlanceBot/kcg/check.py
@@ -0,0 +1,28 @@
+import requests
+
+check_payload = {
+    '__EVENTTARGET' : '' ,
+    '__EVENTARGUMENT' : '',
+    '__LASTFOCUS' : '',
+    '__VIEWSTATE' : '/wEPDwUKMTQ4NjQwMTIzNw9kFgICAw9kFgoCCQ8QDxYGHg1EYXRhVGV4dEZpZWxkBQhjb2xsbmFtZR4ORGF0YVZhbHVlRmllbGQFDGNvbGxlZ2VfY29kZR4LXyFEYXRhQm91bmRnZBAVARlLQ0cgQ29sbGVnZSBvZiBUZWNobm9sb2d5FQECMTMUKwMBZxYBZmQCCw8QZBAVAgtSb2xsIE51bWJlcg9SZWdpc3RlciBOdW1iZXIVAgEwATEUKwMCZ2cWAWZkAg0PD2QWBB4LcGxhY2Vob2xkZXIFC1JvbGwgTnVtYmVyHgxhdXRvY29tcGxldGUFA29mZmQCEQ8PFgIeB1Zpc2libGVoFgIfBAUDb2ZmZAIVDw8WAh8FaGRkZNW+28hPSHWELbHwTZyc+FgrCQj/p6TzJx0gJo3tNhyA', 
+    '__VIEWSTATEGENERATOR' : 'CA0B0334',
+    '__EVENTVALIDATION' : '/wEdAAa5cfVM3pWzdu9rE2vQn04A1ewWtm3evXPJ0S9N/1pup/olUdBTEtKbUYVn9qLUVnP36l7NJf9XLe0xTP1byily7ATayzSAKKfWGUr2Dqcb+c34O/GfAV4V4n0wgFZHr3fbr4+GviYj6YKdFlGPdh5Q23daRHDXkik+zyEsEtmUSg==',
+    'rblOnlineAppLoginMode' : None,
+    'txtuname' : None,
+    'Button1' : 'Login'
+    }
+
+def check_id(user_id):
+    check_payload['txtuname'] = user_id
+    
+    check_payload['rblOnlineAppLoginMode'] = 0
+    page = requests.post('http://studentonlinepayment.kcgcollege.ac.in/', data = check_payload)
+    if page.url != 'http://studentonlinepayment.kcgcollege.ac.in/':
+        return True
+    
+    check_payload['rblOnlineAppLoginMode'] = 1
+    page = requests.post('http://studentonlinepayment.kcgcollege.ac.in/', data = check_payload)
+    if page.url != 'http://studentonlinepayment.kcgcollege.ac.in/':
+        return True
+    
+    return False
---
+++ DarkGlanceBot/main.py
@@ -0,0 +1,53 @@
+#DarkGlanceBot
+
+import discord
+from discord.ext import commands
+import darkglance
+from kcg.student_login import kcg_student, driver 
+from kcg.finddob import find_dob
+from kcg.check import *
+import os #to get current working directory
+
+client = commands.Bot(command_prefix = '.')
+
+@client.event
+async def on_ready():
+    print("DarkGlanceBot has been started")
+
+@client.command()
+async def hello(text):
+    await text.send('Hi')
+    
+@client.command()   
+async def kcg(text):
+    command = text.message.content.split()
+    year = '0000'
+    try:
+        if command[1] == 'photo':
+            if check_id(command[2]):
+                
+                user_id = command[2]
+                try:
+                    year = command[3] 
+                except:
+                    None              
+                d_o_b = find_dob(user_id, year)
+                
+                kcg_student.login(user_id, d_o_b)
+
+                kcg_student.get_photo(path = os.getcwd() + '\kcg', uid = user_id)
+                
+            else:
+                print('hi main')
+    except:
+        None       
+            
+        
+
+    
+
+
+
+client.run(darkglance.discord.token)
+
+
