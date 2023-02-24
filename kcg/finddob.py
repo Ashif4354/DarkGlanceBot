70a6fcc00ddf7b0ad43f53bb890bb0388828ba9b
A Ashif <53007623+Ashif4354@users.noreply.github.com>
2023-02-24 18:57:43 +0530

    payload updated

--- kcg/finddob.py
+++ kcg/finddob.py
@@ -8,17 +8,16 @@ from darkglance import DobNotFound
 student_login_url = 'http://studentlogin.kcgcollege.ac.in/'
 
 student_login_payload = {
-    '__EVENTTARGET' : '' ,
-    '__EVENTARGUMENT' : '',
-    '__LASTFOCUS' : '',
-    '__VIEWSTATE' : '/wEPDwUJMjkwMTA2NTY5D2QWAgIDD2QWCgIDDxAPFgYeDURhdGFUZXh0RmllbGQFCGNvbGxuYW1lHg5EYXRhVmFsdWVGaWVsZAUMY29sbGVnZV9jb2RlHgtfIURhdGFCb3VuZGdkEBUBGUtDRyBDb2xsZWdlIG9mIFRlY2hub2xvZ3kVAQIxMxQrAwFnFgFmZAIFDxBkEBUCC1JvbGwgTnVtYmVyEVJlZ2lzdGVyZWQgTnVtYmVyFQIBMAExFCsDAmdnFgFmZAIHDw9kFgQeC3BsYWNlaG9sZGVyBQtSb2xsIE51bWJlch4MYXV0b2NvbXBsZXRlBQNvZmZkAgsPD2QWAh8EBQNvZmZkAg8PDxYCHgdWaXNpYmxlaGRkZEUh8Q9VeEnmpvJTjWVIwQmtVpX5IBYcjkAZZqWYNv5m', 
-    '__VIEWSTATEGENERATOR' : 'CA0B0334',
-    '__EVENTVALIDATION' : '/wEdAAfEhVpMiIC9PlqrGxNesSta1ewWtm3evXPJ0S9N/1pup/olUdBTEtKbUYVn9qLUVnP36l7NJf9XLe0xTP1byily7ATayzSAKKfWGUr2Dqcb+ZxpWckI3qdmfEJVCu2f5cHN+DvxnwFeFeJ9MIBWR6935FJfAFbS62yyYTlq6hIkdlrWUyRFAO0MmBe4dmPHJe8=',
-    'rblOnlineAppLoginMode' : '0',
-    'txtuname' : None,
-    'txtpassword' : None,
-    'Button1' : 'Login'
-}
+        '__EVENTTARGET' : '' ,
+        '__EVENTARGUMENT' : '',
+        '__LASTFOCUS' : '',
+        '__VIEWSTATE' : '/wEPDwUJMjkwMTA2NTY5D2QWAgIDD2QWCgIDDxAPFgYeDURhdGFUZXh0RmllbGQFCGNvbGxuYW1lHg5EYXRhVmFsdWVGaWVsZAUMY29sbGVnZV9jb2RlHgtfIURhdGFCb3VuZGdkEBUBGUtDRyBDb2xsZWdlIG9mIFRlY2hub2xvZ3kVAQIxMxQrAwFnFgFmZAIFDxBkEBUCC1JvbGwgTnVtYmVyEVJlZ2lzdGVyZWQgTnVtYmVyFQIBMAExFCsDAmdnFgFmZAIHDw9kFgQeC3BsYWNlaG9sZGVyBQtSb2xsIE51bWJlch4MYXV0b2NvbXBsZXRlBQNvZmZkAgsPD2QWAh8EBQNvZmZkAg8PDxYCHgdWaXNpYmxlaGRkZP8sQxS46q1mNTMVnB8XKmifm4muh93nP685dZM0dyyi',
+        '__EVENTVALIDATION' : '/wEdAAerjpyqZPhQVi0g0sCSp89g1ewWtm3evXPJ0S9N/1pup/olUdBTEtKbUYVn9qLUVnP36l7NJf9XLe0xTP1byily7ATayzSAKKfWGUr2Dqcb+ZxpWckI3qdmfEJVCu2f5cHN+DvxnwFeFeJ9MIBWR6938qKBsMtBfVKztnpaWRmPF+AhsCuOEtmW/K8pPRoRI0o=',
+        'rblOnlineAppLoginMode' : None,
+        'txtuname' : None,
+        'txtpassword' : None,
+        'Button1' : 'Login'
+        }
 
 time_out_dates = [] 
 
